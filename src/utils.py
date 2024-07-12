9d4042b91ee8dd3adb51b44fcb4d04e5ac323b69
Juanfra Leonhardt <juanfraleonhardt@gmail.com>
2024-07-11 23:53:44 -0400

    Add EDA plot

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,24 @@
 import matplotlib.pyplot as plt
 
+def plot_relationships(data, x_column):
+    # Set up the plot
+    fig, axes = plt.subplots(2, 3, figsize=(16, 8))
+    axes = axes.flatten()
+
+    # Create square heatmaps using hexbin
+    variables = ['temp', 'humidity', 'precip', 'lw_avg_temp', 'lw_avg_humidity', 'lw_precip']
+    for i, var in enumerate(variables):
+        x = data[var]
+        y = data[x_column]
+        ax = axes[i]
+        hb = ax.hexbin(x, y, gridsize=40, cmap='viridis', mincnt=1)
+        ax.set_title(f'{var} vs {x_column}')
+        fig.colorbar(hb, ax=ax)
+
+    # Adjust layout and show the plot
+    plt.tight_layout()
+    plt.show()
+
 
 def plot_results(y_test, y_pred):
     plt.figure(figsize=(16, 6))
