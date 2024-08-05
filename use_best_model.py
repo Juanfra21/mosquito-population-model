2951b1e72acdf3e577243ff8c57e5d204bb9b91c
Juanfra Leonhardt <juanfraleonhardt@gmail.com>
2024-08-05 03:08:49 -0400

    Fix

--- use_best_model.py
+++ use_best_model.py
@@ -2,7 +2,7 @@ from src.selected_model import train_best_model, test_on_other_datasets
 from src.config import CONFIG
 
 def main():
-    model_key = CONFIG["model_key"]
+    model_key = CONFIG["best_model_key"]
     data_testing_path = CONFIG["data_testing_path"]
 
     model = train_best_model(model_key, save = False)
