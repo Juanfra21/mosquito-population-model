58d0dea5cf2444228e6743b0080df04f9e963b3e
Juanfra Leonhardt <juanfraleonhardt@gmail.com>
2024-08-05 02:37:53 -0400

    Change to weekly basis

--- src/data_processing.py
+++ src/data_processing.py
@@ -31,39 +31,25 @@ def load_population_data(population_path):
 
 
 def interpolate_population(population_df):
+    # Create a yearweek column
+    population_df["date"] = population_df['date'].dt.isocalendar().year.astype(str) + "_" + population_df['date'].dt.isocalendar().week.astype(str).str.zfill(2)
+
+    # Group by date column
+    population_df = population_df.groupby("date").sum().reset_index()
+
     # Sort by date
-    population_df = population_df.sort_values("date")
-
-    # Set the date column as index
-    population_df.set_index("date", inplace=True)
-
-    # Create a date range from the first day until the last day
-    date_range = pd.date_range(
-        start=population_df.index.min(), end=population_df.index.max(), freq="D"
-    )
-
-    # Use the date range as new index
-    population_df = population_df.reindex(date_range)
-
-    # Interpolate to fill population values
-    population_df = population_df.interpolate(method="linear")
-
-    # Reset the index and turn the dates back into a column
-    population_df.reset_index(inplace=True)
-
-    # Rename the columns appropriately
-    population_df.rename(columns={"index": "date"}, inplace=True)
+    population_df.sort_values("date")
 
     return population_df
 
 
 def create_future_population(population_df):
-    # Calculate the average population of the current day and the next two days
+    # Calculate the average population of the current day and the next two weeks
     population_df["population_2d"] = (
         population_df["population"].rolling(window=2, min_periods=1).mean().shift(-1)
     )
 
-    # Calculate the average population of the current day and the next three days
+    # Calculate the average population of the current day and the next three weeks
     population_df["population_3d"] = (
         population_df["population"].rolling(window=3, min_periods=1).mean().shift(-2)
     )
@@ -82,38 +68,44 @@ def generate_weather_data(weather_path):
     # Select necessary column
     weather_df = weather_df[CONFIG["data"]["selected_weather_columns"]]
 
-    # Average temperature in the last week
-    weather_df["lw_avg_temp"] = weather_df["temp"].rolling(window=7).mean()
+    # Create yearweek column
+    weather_df["datetime"] = weather_df['datetime'].dt.isocalendar().year.astype(str) + "_" + weather_df['datetime'].dt.isocalendar().week.astype(str).str.zfill(2)
 
-    # Average humidity in the last week
-    weather_df["lw_avg_humidity"] = weather_df["humidity"].rolling(window=7).mean()
+    # Group by datetime column
+    weather_df = weather_df.groupby("datetime").agg({'precip': 'sum','humidity': 'mean','temp': 'mean'}).reset_index()
 
-    # Precipitation in the last week
-    weather_df["lw_precip"] = weather_df["precip"].rolling(window=7).sum()
+    # Average temperature in the last 3 weeks
+    weather_df["lw_avg_temp"] = weather_df["temp"].rolling(window=3).mean()
+
+    # Average humidity in the last 3 weeks
+    weather_df["lw_avg_humidity"] = weather_df["humidity"].rolling(window=3).mean()
+
+    # Precipitation in the last 3 weeks
+    weather_df["lw_precip"] = weather_df["precip"].rolling(window=3).sum()
 
     # Rename the columns appropriately
     weather_df.rename(columns={"datetime": "date"}, inplace=True)
-
     return weather_df
 
 
-def merge_population_and_weather(population_df, weather_df, shift):
+def merge_population_and_weather(population_df, weather_df, population_rolling_window, shift=True):
     # Shift weather data by 1 day
     if shift:
-        weather_df["date"] = weather_df["date"] + pd.DateOffset(days=1)
+        # Shift all columns except the first one down by 1 row
+        weather_df.iloc[1:] = weather_df.iloc[:-1].values
 
     # Merge population and weather data
     data = pd.merge(population_df, weather_df, on="date", how="left")
 
     # Smooth data using a 3 week rolling window
-    data["population"] = data["population"].rolling(window=28).mean().fillna(method='ffill')
+    data["population"] = data["population"].rolling(window = population_rolling_window).mean().fillna(method='ffill')
 
     return data
 
 
-def prepare_data():
-    population_df = interpolate_population(load_population_data(CONFIG["data"]["population_path"]))
+def prepare_data(population_path, weather_path, population_rolling_window):
+    population_df = interpolate_population(load_population_data(population_path))
     population_df = create_future_population(population_df)
-    weather_df = generate_weather_data(CONFIG["data"]["weather_path"])
-    
-    return merge_population_and_weather(population_df, weather_df, shift=True)
+    weather_df = generate_weather_data(weather_path)
+
+    return merge_population_and_weather(population_df, weather_df, population_rolling_window)
